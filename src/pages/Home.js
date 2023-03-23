f3f724c6bacee1eec5c3f9eebaeccd65e5d13566
Russell Fraze <russell.fraze@gmail.com>
2023-03-23 15:25:20 -0700

    header image layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,24 @@
 import styles from './Home.module.css'
 import Button from '../UI/Button'
+import headerImage from '../assets/home/mobile/image-header.jpg'
 
 
 function Home() {
 
 
     return (
-        
+
         <div className={`${styles.headerImage} ${'flex_group_column'}`}>
-            <p className='overline'>New product</p>
-            <h3>XX99 Mark II Headphones</h3>
-            <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-            <Button className={'buttonMain'}>see product</Button>
+            <img src={headerImage} />
+            <div className={styles.headerImage_content}>
+                <p className='overline'>New product</p>
+                <h3>XX99 Mark II Headphones</h3>
+                <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                <Button className={'buttonMain'}>see product</Button>
+            </div>
 
         </div>
-       
+
     )
 }
 
