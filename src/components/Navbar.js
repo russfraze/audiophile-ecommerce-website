c63a8495b4e8b575f9c7f4ea5db00f68a0cca7c0
Russell Fraze <russell.fraze@gmail.com>
2023-03-30 16:09:57 -0700

    navbar display logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,45 @@
+import { useState } from 'react'
 import styles from './Navbar.module.css'
-import { ReactComponent as CartIcon} from '../assets/shared/desktop/icon-cart.svg'
-import { ReactComponent as Burger} from '../assets/shared/tablet/icon-hamburger.svg'
-import { ReactComponent as Logo} from '../assets/shared/desktop/logo.svg'
+import { ReactComponent as CartIcon } from '../assets/shared/desktop/icon-cart.svg'
+import { ReactComponent as Burger } from '../assets/shared/tablet/icon-hamburger.svg'
+import { ReactComponent as Logo } from '../assets/shared/desktop/logo.svg'
+
+
 
 function Navbar() {
+    const [navIsOpen, setNavIsOpen] = useState(false)
+
+    const burgerClick = () => {
+        setNavIsOpen(!navIsOpen)
+        console.log(navIsOpen)
+    }
+
     return (
-        <nav className={styles.navBar}>
-            <Burger className={styles.burger}/>
-            <Logo className={styles.logo}/>
-            <ul>
-                <li>home</li>
-                <li>headphones</li>
-                <li>speakers</li>
-                <li>earphones</li>
-            </ul>
-            <CartIcon className={styles.cart}/>
-            
-        </nav>
+        <>
+            <nav className={styles.navBar}>
+                <Burger onClick={burgerClick} className={styles.burger} />
+                <Logo className={styles.logo} />
+                <ul>
+                    <li>home</li>
+                    <li>headphones</li>
+                    <li>speakers</li>
+                    <li>earphones</li>
+                </ul>
+                <CartIcon className={styles.cart} />
+
+            </nav>
+            { navIsOpen ? <div className={styles.dropdown} >
+                <div className={styles.dummyObject}>
+
+                </div>
+                <div className={styles.dummyObject}>
+
+                </div>
+                <div className={styles.dummyObject}>
+
+                </div>
+            </div> : ''}
+        </>
     )
 }
 
