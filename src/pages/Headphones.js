2c7982758aa5ccb0d142c3a6ee9ad5bb12c7fb39
Russell Fraze <russell.fraze@gmail.com>
2023-03-31 13:51:25 -0700

    categories layout experiment progress

--- src/pages/Headphones.js
+++ src/pages/Headphones.js
@@ -1,12 +1,23 @@
 import styles from '../pages/Headphones.module.css'
+import headphonesOne from '../assets/shared/desktop/image-category-thumbnail-headphones.png'
+import earphonesCase from '../assets/shared/desktop/image-category-thumbnail-earphones.png'
+import speakerZx9 from '../assets/home/mobile/image-speaker-zx9.png'
 
 function Headphones() {
     return (
         <article>
-
-            <div className={`${styles.headphones} ${'card'} ${'bG_grey'}`}></div>
-            <div className={`${styles.speakers} ${'card'} ${'bG_grey'}`}></div>
-            <div className={`${styles.earphones} ${'card'} ${'bG_grey'}`}></div>
+            <div className={styles.container}>
+                <img className={styles.headphonesImage} src={headphonesOne} />
+                <div className={`${styles.headphones} ${'bG_grey'}`}></div>
+            </div>
+            <div className={styles.container}>
+                <img className={styles.speakersImage} src={speakerZx9} />
+                <div className={`${styles.speakers} ${'bG_grey'}`}></div>
+            </div>
+            <div className={styles.container}>
+                <img className={styles.earphonesCase} src={earphonesCase} />
+                <div className={`${styles.earphones} ${'bG_grey'}`}></div>
+            </div>
 
 
         </article>
