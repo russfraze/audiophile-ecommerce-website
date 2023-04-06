3bab8a9df841ef61d53ac2d033a6dd02efe74681
Russell Fraze <russell.fraze@gmail.com>
2023-04-06 12:41:25 -0700

    earphones page layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Headphones from './pages/Headphones'
 import Speakers from './pages/Speakers'
+import Earphones from './pages/Earphones'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/headphones' element={<Headphones />} />
           <Route path='/speakers' element={<Speakers />} />
+          <Route path='/earphones' element={<Earphones />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Earphones.js
@@ -0,0 +1,35 @@
+import styles from './Earphones.module.css'
+
+import yx1 from '../assets/product-yx1-earphones/mobile/image-product.jpg'
+import Button from '../UI/Button'
+import Categories from '../components/Categories'
+import BestAudio from '../components/BestAudio'
+
+
+function Earphones() {
+    return (
+        <>
+            <header>
+                <h2>earphones</h2>
+            </header>
+
+            <main>
+                <img src={yx1} />
+                <p>new product</p>
+                <h2>yx1 wireless earphones</h2>
+                <p>Tailor your listening experience with bespoke dynamic drivers from the new 
+                    YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy 
+                    environments with its active noise cancellation feature.
+                </p>
+                <Button>see product</Button>
+
+                <Categories />
+                <BestAudio />
+
+            </main>
+
+        </>
+    )
+}
+
+export default Earphones
