427c379b30007db81192db6b29eb31182a258f4c
Russell Fraze <russell.fraze@gmail.com>
2023-04-06 10:55:42 -0700

    best audio module added

---
+++ src/components/BestAudio.js
@@ -0,0 +1,30 @@
+import styles from './BestAudio.module.css'
+import bestGear from '../assets/shared/mobile/image-best-gear.jpg'
+import bestGearTab from '../assets/shared/tablet/image-best-gear.jpg'
+
+
+function BestAudio() {
+    return (
+        <section>
+
+                <div className={styles.bestImage}>
+                    <picture>
+                        <source media="(min-width:768px)" srcset={bestGearTab} />
+                        <img src={bestGear} />
+                    </picture>
+                </div>
+
+                <div className={`${styles.bestImage__text} ${'flex_group_column'}`}>
+                    <h2 className='black'>Bringing you the <span className={styles.wordPop}>best</span> audio gear</h2>
+                    <p className='black50'> Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+                    earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
+                    rooms available for you to browse and experience a wide range of our products. Stop by our store
+                    to meet some of the fantastic people who make Audiophile the best place to buy your portable
+                    audio equipment.</p>
+                </div>
+
+            </section>
+    )
+}
+
+export default BestAudio
