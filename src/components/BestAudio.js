ca10871791c0dea68a2f1cb8a6497bdc30d4c15f
Russell Fraze <russell.fraze@gmail.com>
2023-04-12 12:39:58 -0700

    mark2 product page tablet layout finish

--- src/components/BestAudio.js
+++ src/components/BestAudio.js
@@ -5,25 +5,25 @@ import bestGearTab from '../assets/shared/tablet/image-best-gear.jpg'
 
 function BestAudio() {
     return (
-        <section>
+        <section className={styles.bestAudio}>
 
-                <div className={styles.bestImage}>
-                    <picture>
-                        <source media="(min-width:768px)" srcset={bestGearTab} />
-                        <img src={bestGear} />
-                    </picture>
-                </div>
+            <div className={styles.bestAudio__image}>
+                <picture>
+                    <source media="(min-width:768px)" srcset={bestGearTab} />
+                    <img src={bestGear} />
+                </picture>
+            </div>
 
-                <div className={`${styles.bestImage__text} ${'flex_group_column'}`}>
-                    <h2 className='black'>Bringing you the <span className={styles.wordPop}>best</span> audio gear</h2>
-                    <p className='black50'> Located at the heart of New York City, Audiophile is the premier store for high end headphones,
-                    earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
-                    rooms available for you to browse and experience a wide range of our products. Stop by our store
-                    to meet some of the fantastic people who make Audiophile the best place to buy your portable
+            <div className={`${styles.bestAudio__text} ${'flex_group_column'}`}>
+                <h2 className='black'>Bringing you the <span className={styles.wordPop}>best</span> audio gear</h2>
+                <p className='black50'> Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+                earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
+                rooms available for you to browse and experience a wide range of our products. Stop by our store
+                to meet some of the fantastic people who make Audiophile the best place to buy your portable
                     audio equipment.</p>
-                </div>
+            </div>
 
-            </section>
+        </section>
     )
 }
 
--- src/pages/Mark2.js
+++ src/pages/Mark2.js
@@ -1,5 +1,6 @@
 import styles from './Mark2.module.css'
 import Mark2Image from '../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg'
+import Mark2ImageTab from '../assets/product-xx99-mark-two-headphones/tablet/image-product.jpg'
 import gallery1 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg'
 import gallery2 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-2.jpg'
 import gallery3 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-3.jpg'
@@ -9,9 +10,12 @@ import gallery3Tab from '../assets/product-xx99-mark-two-headphones/tablet/image
 import gallery1Desk from '../assets/product-xx99-mark-two-headphones/desktop/image-gallery-1.jpg'
 import gallery2Desk from '../assets/product-xx99-mark-two-headphones/desktop/image-gallery-2.jpg'
 import gallery3Desk from '../assets/product-xx99-mark-two-headphones/desktop/image-gallery-3.jpg'
-import markII from '../assets/shared/mobile/image-xx99-mark-two-headphones.jpg'
+import markI from '../assets/shared/mobile/image-xx99-mark-one-headphones.jpg'
 import xx59 from '../assets/shared/mobile/image-xx59-headphones.jpg'
 import zx7 from '../assets/shared/mobile/image-zx9-speaker.jpg'
+import markITab from '../assets/shared/tablet/image-xx99-mark-one-headphones.jpg'
+import xx59Tab from '../assets/shared/tablet/image-xx59-headphones.jpg'
+import zx7Tab from '../assets/shared/tablet/image-zx9-speaker.jpg'
 import Button from '../UI/Button'
 import NumberButton from '../UI/NumberButton'
 import Categories from '../components/Categories'
@@ -21,98 +25,130 @@ function Mark2() {
     return (
         <main>
             <p className='black50'>Go Back</p>
-            <img className='card' src={Mark2Image} />
-            <p className='overline coral'>new product</p>
-            <h2 className='black'>XX99 Mark II Headphones</h2>
-            <p className='black50'>The new XX99 Mark II headphones is the pinnacle of pristine audio.
-            It redefines your premium headphone experience by reproducing the
-            balanced depth and precision of studio-quality sound.
-                </p>
-            <h6 className='black'>$ 2,999</h6>
 
-            <div className={styles.btnGroup}>
-                <NumberButton />
+            <div className={styles.purchaseInfo}>
+                <picture>
+                    <source media="(min-width:768px)" srcset={Mark2ImageTab} />
+                    <img className='card' src={Mark2Image} />
+                </picture>
 
-                <Button>add to cart</Button>
+                <div className={styles.addToCart}>
+                    <p className='overline coral'>new product</p>
+                    <h4 className='black'>XX99 Mark II Headphones</h4>
+                    <p className='black50'>The new XX99 Mark II headphones is the pinnacle of pristine audio.
+                    It redefines your premium headphone experience by reproducing the
+                    balanced depth and precision of studio-quality sound.
+                    </p>
+                    <h6 className='black'>$ 2,999</h6>
+
+                    <div className={styles.btnGroup}>
+                        <NumberButton />
+
+                        <Button>add to cart</Button>
+                    </div>
+                </div>
             </div>
 
 
 
 
-
-            <h3 className='black'>FEATURES</h3>
-            <p className='black50'>Featuring a genuine leather head strap and premium earcups, these
-            headphones deliver superior comfort for those who like to enjoy endless
-            listening. It includes intuitive controls designed for any situation.
-            Whether you’re taking a business call or just in your own personal space,
-            the auto on/off and pause features ensure that you’ll never miss a beat.
+            <section className={styles.features}>
+                <h3 className='black'>FEATURES</h3>
+                <p className='black50'>Featuring a genuine leather head strap and premium earcups, these
+                headphones deliver superior comfort for those who like to enjoy endless
+                listening. It includes intuitive controls designed for any situation.
+                Whether you’re taking a business call or just in your own personal space,
+            the auto on/off and pause features ensure that you’ll never miss a beat.<br /><br />
 
             The advanced Active Noise Cancellation with built-in equalizer allow you
             to experience your audio world on your terms. It lets you enjoy your audio
             in peace, but quickly interact with your surroundings when you need to.
             Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery
             life, the XX99 Mark II headphones gives you superior sound, cutting-edge
-                    technology, and a modern design aesthetic.</p>
+            technology, and a modern design aesthetic.
+            </p>
+            </section>
 
-            <h3 className='black'>In the box</h3>
 
 
-            <div className={styles.inBox}>
+            <section className={styles.inBox}>
+                <h3 className='black'>In the box</h3>
+                <div className={styles.inBox__list}>
 
-                <div className='flex_group'>
-                    <p className={styles.amount}>1x</p>
-                    <p className='black50'>Headphone Unit</p>
+                    <div className='flex_group'>
+                        <p className={styles.amount}>1x</p>
+                        <p className='black50'>Headphone Unit</p>
+                    </div>
+                    <div className='flex_group'>
+                        <p className={styles.amount}>2x</p>
+                        <p className='black50'>Replacement Earcups</p>
+                    </div>
+                    <div className='flex_group'>
+                        <p className={styles.amount}>1x</p>
+                        <p className='black50'>User Manual</p>
+                    </div>
+                    <div className='flex_group'>
+                        <p className={styles.amount}>1x</p>
+                        <p className='black50'>3.5mm 5m Audio Cable</p>
+                    </div>
+                    <div className='flex_group'>
+                        <p className={styles.amount}>1x</p>
+                        <p className='black50'>Travel Bag</p>
+                    </div>
                 </div>
-                <div className='flex_group'>
-                    <p className={styles.amount}>2x</p>
-                    <p className='black50'>Replacement Earcups</p>
-                </div>
-                <div className='flex_group'>
-                    <p className={styles.amount}>1x</p>
-                    <p className='black50'>User Manual</p>
-                </div>
-                <div className='flex_group'>
-                    <p className={styles.amount}>1x</p>
-                    <p className='black50'>3.5mm 5m Audio Cable</p>
-                </div>
-                <div className='flex_group'>
-                    <p className={styles.amount}>1x</p>
-                    <p className='black50'>Travel Bag</p>
-                </div>
-            </div>
+            </section>
 
-            <picture>
-                <source media="(min-width:768px)" srcset={gallery1Tab} />
-                <img className='card' src={gallery1} />
-            </picture>
+            <section className={styles.gallery}>
 
-            <picture>
-                <source media="(min-width:768px)" srcset={gallery2Tab} />
-                <img className='card' src={gallery2} />
-            </picture>
+                <div className={styles.gallery__lifestyle}>
+                    <picture>
+                        <source media="(min-width:768px)" srcset={gallery1Tab} />
+                        <img className='card' src={gallery1} />
+                    </picture>
 
-            <picture>
-                <source media="(min-width:768px)" srcset={gallery3Tab} />
-                <img className='card' src={gallery3} />
-            </picture>
+                    <picture>
+                        <source media="(min-width:768px)" srcset={gallery2Tab} />
+                        <img className='card' src={gallery2} />
+                    </picture>
+
+                </div>
+
+                <picture>
+                    <source media="(min-width:768px)" srcset={gallery3Tab} />
+                    <img className='card' src={gallery3} />
+                </picture>
+            </section>
 
             <section className={`${styles.alsoLike} ${'flex_group_column'}`}>
                 <h3 className='black'>you may also like</h3>
-                <picture>
-                    <img className='card' src={markII} />
-                </picture>
-                <h3 className='black'>xx99 mark I</h3>
-                <Button>see product</Button>
-                <picture>
-                    <img className='card' src={xx59} />
-                </picture>
-                <h3 className='black'>xx59</h3>
-                <Button>see product</Button>
-                <picture>
-                    <img className='card' src={zx7} />
-                </picture>
-                <h3 className='black'>zx9 speaker</h3>
-                <Button>see product</Button>
+                <div className={styles.alsoLike__products}>
+                    <div className={styles.alsoLike__product}>
+                        <picture>
+                            <source media="(min-width:768px)" srcset={markITab} />
+                            <img className='card' src={markI} />
+                        </picture>
+                        <h5 className='black'>xx99 mark I</h5>
+                        <Button>see product</Button>
+                    </div>
+
+                    <div className={styles.alsoLike__product}>
+                        <picture>
+                            <source media="(min-width:768px)" srcset={xx59Tab} />
+                            <img className='card' src={xx59} />
+                        </picture>
+                        <h5 className='black'>xx59</h5>
+                        <Button>see product</Button>
+                    </div>
+
+                    <div className={styles.alsoLike__product}>
+                        <picture>
+                            <source media="(min-width:768px)" srcset={zx7Tab} />
+                            <img className='card' src={zx7} />
+                        </picture>
+                        <h5 className='black'>zx9 speaker</h5>
+                        <Button>see product</Button>
+                    </div>
+                </div>
             </section>
 
             <Categories />
