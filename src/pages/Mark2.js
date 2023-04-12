7957199df10a7392e166a5b0e202e97cc5c24762
Russell Fraze <russell.fraze@gmail.com>
2023-04-12 15:45:29 -0700

    mark2 detail page desktop layout start

--- src/pages/Mark2.js
+++ src/pages/Mark2.js
@@ -1,6 +1,7 @@
 import styles from './Mark2.module.css'
 import Mark2Image from '../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg'
 import Mark2ImageTab from '../assets/product-xx99-mark-two-headphones/tablet/image-product.jpg'
+import Mark2ImageDesk from '../assets/product-xx99-mark-two-headphones/desktop/image-product.jpg'
 import gallery1 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg'
 import gallery2 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-2.jpg'
 import gallery3 from '../assets/product-xx99-mark-two-headphones/mobile/image-gallery-3.jpg'
@@ -28,6 +29,7 @@ function Mark2() {
 
             <div className={styles.purchaseInfo}>
                 <picture>
+                    <source media="(min-width:1440px)" srcset={Mark2ImageDesk} />
                     <source media="(min-width:768px)" srcset={Mark2ImageTab} />
                     <img className='card' src={Mark2Image} />
                 </picture>
@@ -50,63 +52,67 @@ function Mark2() {
             </div>
 
 
+            <div className={styles.productDetails}>
+
+                <section className={styles.features}>
+                    <h3 className='black'>FEATURES</h3>
+                    <p className='black50'>Featuring a genuine leather head strap and premium earcups, these
+                    headphones deliver superior comfort for those who like to enjoy endless
+                    listening. It includes intuitive controls designed for any situation.
+                    Whether you’re taking a business call or just in your own personal space,
+                    the auto on/off and pause features ensure that you’ll never miss a beat.<br /><br />
+
+                    The advanced Active Noise Cancellation with built-in equalizer allow you
+                    to experience your audio world on your terms. It lets you enjoy your audio
+                    in peace, but quickly interact with your surroundings when you need to.
+                    Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery
+                    life, the XX99 Mark II headphones gives you superior sound, cutting-edge
+                    technology, and a modern design aesthetic.
+                    </p>
+                </section>
 
 
-            <section className={styles.features}>
-                <h3 className='black'>FEATURES</h3>
-                <p className='black50'>Featuring a genuine leather head strap and premium earcups, these
-                headphones deliver superior comfort for those who like to enjoy endless
-                listening. It includes intuitive controls designed for any situation.
-                Whether you’re taking a business call or just in your own personal space,
-            the auto on/off and pause features ensure that you’ll never miss a beat.<br /><br />
 
-            The advanced Active Noise Cancellation with built-in equalizer allow you
-            to experience your audio world on your terms. It lets you enjoy your audio
-            in peace, but quickly interact with your surroundings when you need to.
-            Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery
-            life, the XX99 Mark II headphones gives you superior sound, cutting-edge
-            technology, and a modern design aesthetic.
-            </p>
-            </section>
+                <section className={styles.inBox}>
+                    <h3 className='black'>In the box</h3>
+                    <div className={styles.inBox__list}>
 
-
-
-            <section className={styles.inBox}>
-                <h3 className='black'>In the box</h3>
-                <div className={styles.inBox__list}>
-
-                    <div className='flex_group'>
-                        <p className={styles.amount}>1x</p>
-                        <p className='black50'>Headphone Unit</p>
+                        <div className='flex_group'>
+                            <p className={styles.amount}>1x</p>
+                            <p className='black50'>Headphone Unit</p>
+                        </div>
+                        <div className='flex_group'>
+                            <p className={styles.amount}>2x</p>
+                            <p className='black50'>Replacement Earcups</p>
+                        </div>
+                        <div className='flex_group'>
+                            <p className={styles.amount}>1x</p>
+                            <p className='black50'>User Manual</p>
+                        </div>
+                        <div className='flex_group'>
+                            <p className={styles.amount}>1x</p>
+                            <p className='black50'>3.5mm 5m Audio Cable</p>
+                        </div>
+                        <div className='flex_group'>
+                            <p className={styles.amount}>1x</p>
+                            <p className='black50'>Travel Bag</p>
+                        </div>
                     </div>
-                    <div className='flex_group'>
-                        <p className={styles.amount}>2x</p>
-                        <p className='black50'>Replacement Earcups</p>
-                    </div>
-                    <div className='flex_group'>
-                        <p className={styles.amount}>1x</p>
-                        <p className='black50'>User Manual</p>
-                    </div>
-                    <div className='flex_group'>
-                        <p className={styles.amount}>1x</p>
-                        <p className='black50'>3.5mm 5m Audio Cable</p>
-                    </div>
-                    <div className='flex_group'>
-                        <p className={styles.amount}>1x</p>
-                        <p className='black50'>Travel Bag</p>
-                    </div>
-                </div>
-            </section>
+                </section>
+
+            </div>
 
             <section className={styles.gallery}>
 
                 <div className={styles.gallery__lifestyle}>
                     <picture>
+                        <source media="(min-width:1440px)" srcset={gallery1Desk} />
                         <source media="(min-width:768px)" srcset={gallery1Tab} />
                         <img className='card' src={gallery1} />
                     </picture>
 
                     <picture>
+                        <source media="(min-width:1440px)" srcset={gallery2Desk} />
                         <source media="(min-width:768px)" srcset={gallery2Tab} />
                         <img className='card' src={gallery2} />
                     </picture>
@@ -114,6 +120,7 @@ function Mark2() {
                 </div>
 
                 <picture>
+                    <source media="(min-width:1440px)" srcset={gallery3Desk} />
                     <source media="(min-width:768px)" srcset={gallery3Tab} />
                     <img className='card' src={gallery3} />
                 </picture>
