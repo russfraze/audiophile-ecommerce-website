09a9a800292735c59310d71b300b78766def01d8
Russell Fraze <russell.fraze@gmail.com>
2023-04-28 10:27:49 -0700

    checkout shipping info form validation

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,35 +2,52 @@ import { useState } from 'react'
 import styles from './Checkout.module.css'
 import Button from '../UI/Button'
 function Checkout() {
-
-
     const [enteredName, setEnteredName] = useState('')
     const [nameTouched, setNameTouched] = useState(false)
     const nameIsValid = enteredName.trim() !== ''
     const enteredNameIsInvalid = !nameIsValid && nameTouched
-
     const [enteredEmail, setEnteredEmail] = useState('')
     const [emailTouched, setEmailTouched] = useState(false)
     const emailIsValid = enteredEmail.trim() !== '' && enteredEmail.includes('@')
     const enteredEmailIsInvalid = !emailIsValid && emailTouched
-
     const [enteredPhone, setEnteredPhone] = useState('')
     const [phoneTouched, setPhoneTouched] = useState(false)
     const phoneIsValid = enteredPhone.trim().length == 10
     const enteredPhoneIsInvalid = !phoneIsValid && phoneTouched
 
 
+    const [enteredAddress, setEnteredAddress] = useState('')
+    const [addressTouched, setAddressTouched] = useState(false)
+    const addressIsValid = enteredAddress.trim() !== ''
+    const enteredAddressIsInvalid = !addressIsValid && addressTouched
+
+    const [enteredZip, setEnteredZip] = useState('')
+    const [zipTouched, setZipTouched] = useState(false)
+    const zipIsValid = enteredZip.trim().length == 5
+    const enteredZipIsInvalid = !zipIsValid && zipTouched
+
+    const [enteredCity, setEnteredCity] = useState('')
+    const [cityTouched, setCityTouched] = useState(false)
+    const cityIsValid = enteredCity.trim() !== ''
+    const enteredCityIsInvalid = !cityIsValid && cityTouched
+
+    const [enteredCountry, setEnteredCountry] = useState('')
+    const [countryTouched, setCountryTouched] = useState(false)
+    const countryIsValid = enteredCountry.trim() !== ''
+    const enteredCountryIsInvalid = !countryIsValid && countryTouched
+
+
+
+
 
 
     const nameInputChangeHandler = (e) => {
         setEnteredName(e.target.value)
-
     }
 
     const nameInputBlurHandler = (e) => {
         setNameTouched(true)
     }
-
 
     //email handeler fucntions:
 
@@ -52,11 +69,50 @@ function Checkout() {
         setPhoneTouched(true)
     }
 
+    //  --------------------- SHIPPING INFO --------------
+
+    const addressInputChangeHandler = (e) => {
+        setEnteredAddress(e.target.value)
+    }
+
+    const addressInputBlurHandler = () => {
+        setAddressTouched(true)
+    }
+
+    const zipInputChangeHandler = (e) => {
+        setEnteredZip(e.target.value)
+    }
+
+    const zipInputBlurHandler = () => {
+        setZipTouched(true)
+    }
+
+    const cityInputChangeHandeler = (e) => {
+        setEnteredCity(e.target.value)
+    }
+
+    const cityInputBlurHandler = () => {
+        setCityTouched(true)
+    }
+
+    const countryInputChangeHandeler = (e) => {
+        setEnteredCountry(e.target.value)
+    }
+
+    const countryInputBlurHandeler = () => {
+        setCountryTouched(true)
+    }
+
     const submitHandler = (e) => {
         e.preventDefault()
 
         setNameTouched(true)
         setEmailTouched(true)
+        setPhoneTouched(true)
+        setAddressTouched(true)
+        setZipTouched(true)
+        setCityTouched(true)
+        setCountryTouched(true)
 
         if (!nameIsValid) {
             return
@@ -68,15 +124,46 @@ function Checkout() {
         }
         setEnteredEmail('')
 
+        if (!phoneIsValid) {
+            return
+        }
+        setEnteredPhone('')
 
+        if (!addressIsValid) {
+            return
+        }
+        setEnteredAddress('')
+        
+        if (!cityIsValid) {
+            return
+        }
+        setEnteredCity('')
+
+        if (!zipIsValid) {
+            return
+        }
+        setEnteredZip('')
+
+        if (!countryIsValid) {
+            return
+        }
+        setEnteredCountry('')
+        
         console.log(enteredName)
         console.log(enteredEmail)
-
-
-
+        console.log(enteredPhone)
+        console.log(enteredAddress)
+        console.log(enteredCity)
+        console.log(enteredZip)
+        console.log(enteredCountry)
 
         setNameTouched(false)
         setEmailTouched(false)
+        setPhoneTouched(false)
+        setAddressTouched(false)
+        setCityTouched(false)
+        setZipTouched(false)
+        setCountryTouched(false)
     }
 
 
@@ -97,42 +184,56 @@ function Checkout() {
                             <input onBlur={nameInputBlurHandler} onChange={nameInputChangeHandler} type='text' value={enteredName} />
                         </div>
 
-                        <div className={styles.checkout__inputGroup}>
-                            <label className='black' htmlFor='name'>Email Address</label>
-                            {enteredEmailIsInvalid && <p className={styles.error}>Please enter valid email</p>}
+                        <div className={enteredEmailIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                            <div className={styles.checkout__labelError}>
+                                <label className='black' htmlFor='name'>Email Address</label>
+                                {enteredEmailIsInvalid && <p className={styles.error}>Please enter valid email</p>}
+                            </div>
                             <input onBlur={emailInputBlurHandeler} type='text' onChange={emailInputChangeHandeler} value={enteredEmail} />
                         </div>
 
-                        <div className={styles.checkout__inputGroup}>
-                            <label className='black' htmlFor='name'>Phone Number</label>
-                            {enteredPhoneIsInvalid && <p className={styles.error}>Please enter valid number</p>}
+                        <div className={enteredPhoneIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                            <div className={styles.checkout__labelError}>
+                                <label className='black' htmlFor='name'>Phone Number</label>
+                                {enteredPhoneIsInvalid && <p className={styles.error}>enter valid number</p>}
+                            </div>
                             <input onBlur={phoneInputBlurHandeler} type='text' onChange={phoneInputChangeHandeler} value={enteredPhone} />
                         </div>
                     </div>
 
 
                     <p className='coral'>SHIPPING INFO</p>
-                    <div className={styles.checkout__inputGroup}>
-                        <label className='black' htmlFor='name'>Your Address</label>
-                        <input type='text' />
+                    <div className={enteredAddressIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                        <div className={styles.checkout__labelError}>
+                            <label className='black' htmlFor='name'>Your Address</label>
+                            {enteredAddressIsInvalid && <p className={styles.error}>Please enter valid address</p>}
+                        </div>
+                        <input onBlur={addressInputBlurHandler} onChange={addressInputChangeHandler} value={enteredAddress} type='text' />
                     </div>
 
-                    <div className={styles.checkout__inputGroup}>
-                        <label className='black' htmlFor='name'>ZIP code</label>
-                        <input type='text' />
+                    <div className={enteredZipIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                        <div className={styles.checkout__labelError}>
+                            <label className='black' htmlFor='name'>ZIP code</label>
+                            {enteredZipIsInvalid && <p className={styles.error}>Please enter valid zipcode</p>}
+                        </div>
+                        <input onBlur={zipInputBlurHandler} onChange={zipInputChangeHandler} value={enteredZip} type='text' />
                     </div>
 
-                    <div className={styles.checkout__inputGroup}>
-                        <label className='black' htmlFor='name'>City</label>
-                        <input type='text' />
+                    <div className={enteredCityIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                        <div className={styles.checkout__labelError}>
+                            <label className='black' htmlFor='name'>City</label>
+                            {enteredCityIsInvalid && <p className={styles.error}>Please enter valid city</p>}
+                        </div>
+                        <input onBlur={cityInputBlurHandler} onChange={cityInputChangeHandeler} value={enteredCity} type='text' />
                     </div>
 
-                    <div className={styles.checkout__inputGroup}>
-                        <label className='black' htmlFor='name'>Country</label>
-                        <input type='text' />
+                    <div className={enteredCountryIsInvalid ? `${styles['checkout__inputGroup--error']}` : `${styles.checkout__inputGroup}`}>
+                        <div className={styles.checkout__labelError}>
+                            <label className='black' htmlFor='name'>Country</label>
+                            {enteredCountryIsInvalid && <p className={styles.error}>Please enter valid country</p>}
+                        </div>
+                        <input onBlur={countryInputBlurHandeler} onChange={countryInputChangeHandeler} value={enteredCountry} type='text' />
                     </div>
-
-
 
                     <p className='coral'>PAYMENT DETAILS</p>
                     <div className={styles.checkout__inputGroup}>
