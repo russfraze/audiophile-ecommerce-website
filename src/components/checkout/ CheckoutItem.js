003380237fc495d4067ba185b9dbcf40c97b232b
Russell Fraze <russell.fraze@gmail.com>
2023-04-30 16:01:49 -0700

    checkout summary finish

---
+++ src/components/checkout/ CheckoutItem.js
@@ -0,0 +1,85 @@
+import styles from './CheckoutItem.module.css'
+import yx1Cart from '../../assets/cart/image-yx1-earphones.jpg'
+import xx59Cart from '../../assets/cart/image-xx59-headphones.jpg'
+import mark1Cart from '../../assets/cart/image-xx99-mark-one-headphones.jpg'
+import mark2Cart from '../../assets/cart/image-xx99-mark-two-headphones.jpg'
+import zx7Cart from '../../assets/cart/image-zx7-speaker.jpg'
+import zx9Cart from '../../assets/cart/image-zx9-speaker.jpg'
+
+const cartImages = [
+    {
+        image: yx1Cart,
+        name: 'YX1'
+    },
+    {
+        image: xx59Cart,
+        name: 'XX59'
+    },
+    {
+        image: mark1Cart,
+        name: 'XX99 MK I'
+    },
+    {
+        image: mark2Cart,
+        name: 'XX99 MK II'
+    },
+    {
+        image: zx7Cart,
+        name: 'ZX7'
+    },
+    {
+        image: zx9Cart,
+        name: 'ZX9'
+    },
+]
+
+function CheckoutItem({ total, id, amount }) {
+
+    let cartImage
+    let cartName
+
+    console.log('rrr', cartImages[1].name)
+    console.log('sss', id)
+
+    if (id === 1) {
+        cartImage = yx1Cart
+        cartName = cartImages[0].name
+    }
+    if (id === 2) {
+        cartImage = xx59Cart
+        cartName = cartImages[1].name
+    }
+    if (id === 3) {
+        cartImage = mark1Cart
+        cartName = cartImages[2].name
+    }
+    if (id === 4) {
+        cartImage = mark2Cart
+        cartName = cartImages[3].name
+    }
+    if (id === 5) {
+        cartImage = zx7Cart
+        cartName = cartImages[4].name
+    }
+    if (id === 6) {
+        cartImage = zx9Cart
+        cartName = cartImages[5].name
+    }
+
+    return (
+        <li className={styles.cartItem}>
+
+            <div className={styles.imageName}>
+                <img className={styles.cartItem__image} src={cartImage} />
+                <div>
+                    <p className={styles.name}>{cartName}</p>
+                    <p className={styles.total}>{total}</p>
+                </div>
+            </div>
+
+            <p className='black50'>{`X${amount}`}</p>
+        </li>
+    )
+}
+
+export default CheckoutItem
--- src/components/checkout/CartSummary.js
+++ src/components/checkout/CartSummary.js
@@ -1,19 +1,40 @@
-import {useContext} from 'react'
+import styles from './CartSummary.module.css'
+import { useContext } from 'react'
 import CartContext from '../../context/CartContext'
-import CartItem from '../cart/CartItem'
+import CheckoutItem from '../checkout/ CheckoutItem'
 
 function CartSummary() {
     const cartData = useContext(CartContext)
-    console.log(cartData.products)
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    const totalFormat = numberWithCommas(cartData.totalAmount)
+    const grandTotal = numberWithCommas(cartData.totalAmount + 50)
+
     return (
         <div>
-            {cartData.products.map(item =>
-                <CartItem
-                    name={item.name}
-                    id={item.id}
-                    total={item.total}
-                    amount={item.amount}
-                />)}
+            <div className={styles.summaryItems}>
+                {cartData.products.map(item =>
+                    <CheckoutItem
+                        name={item.name}
+                        id={item.id}
+                        total={item.total}
+                        amount={item.amount}
+                    />)}
+            </div>
+            <div className={styles.totals}>
+                <div className={styles.total__item}>
+                    <p className='black50'>TOTAL</p> <h6 className='black'>{`$${totalFormat}`}</h6>
+                </div>
+                <div className={styles.total__item}>
+                    <p className='black50'>SHIPPING</p> <h6 className='black'>$50</h6>
+                </div>
+                <div className={styles.total__grand}>
+                    <p className='black50'>GRAND TOTAL</p> <h6 className='coral'>{`$${grandTotal}`}</h6>
+                </div>
+            </div>
         </div>
     )
 }
