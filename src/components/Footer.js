cf6c1c1d76f99fbefa37f7e93203bed520ccf4dc
Russell Fraze <russell.fraze@gmail.com>
2023-05-12 10:00:35 -0700

    footer color pop and spacing adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,25 +10,33 @@ function Footer() {
     const navigate = useNavigate()
     return (
         <footer className={`${styles.footer}`}>
-            <Logo className={styles.logo} />
-            <ul>
-                <li className='subtitle' onClick={() => navigate('/')}>Home</li>
-                <li className='subtitle' onClick={() => navigate('/headphones')}>Headphones</li>
-                <li className='subtitle' onClick={() => navigate('/speakers')}>Speakers</li>
-                <li className='subtitle' onClick={() => navigate('/earphones')}>Earphones</li>
-            </ul>
-            <p className={` ${'white50'}`}>Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers
-            and sound specialists who are devoted to helping you get the most out of personal audio. Come and
-            visit our demo facility - we’re open 7 days a week.</p>
-            <div className={styles.copySocial}>
-                <p className={`${styles.copyRight} ${'white50'}`}>Copyright 2021. All Rights Reserved</p>
-                <div className={`${styles.socialIcons} ${'flex_group'}`}>
-                    <Facebook />
-                    <Twitter />
-                    <Instagram />
+            <div className={styles.footer__content}>
+                <div className={styles.colorPop}></div>
+                <div className={styles.footer__nav}>
+                    <Logo className={styles.logo} />
+                    <ul>
+                        <li className='subtitle' onClick={() => navigate('/')}>Home</li>
+                        <li className='subtitle' onClick={() => navigate('/headphones')}>Headphones</li>
+                        <li className='subtitle' onClick={() => navigate('/speakers')}>Speakers</li>
+                        <li className='subtitle' onClick={() => navigate('/earphones')}>Earphones</li>
+                    </ul>
+                </div>
+
+                <div className={styles.footer__text}>
+                    <p className={` ${'white50'}`}>Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers
+                    and sound specialists who are devoted to helping you get the most out of personal audio. Come and
+                    visit our demo facility - we’re open 7 days a week.</p>
+
+                    <div className={styles.copySocial}>
+                        <p className='white50'>Copyright 2021. All Rights Reserved</p>
+                        <div className={`${styles.socialIcons} ${'flex_group'}`}>
+                            <Facebook className={styles.icon} />
+                            <Twitter className={styles.icon} />
+                            <Instagram className={styles.icon} />
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </footer>
     )
 }
